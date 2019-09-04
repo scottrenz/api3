a16be2832183b428d9c69a5bbcb4e2766ebeabc3
Scott Renz <scottrenz@scottrenz.com>
2019-09-04 14:18:22 -0400

    so far

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 
 
 const express = require('express');
+const productsRouter = require('../products/products-router.js')
+const suppliersRouter = require('../suppliers/suppliers-router.js')
 
 server = express();
 
 server.use(express.json()); // teaches express to parse JSON body
-
+server.use('/products', productsRouter);
+server.use('/suppliers', uppercaser, suppliersRouter)
 
 // sanity check endpoint
 server.get('/', (req, res) => {
@@ -16,6 +19,37 @@ server.get('/suppliers', (req, res) => {
   res.send('got your get to suppliers')
 })
 
+server.get('/suppliers/:name', (req, res) => {
+  res.send('got your get to suppliers')
+})
+
 server.get('/clients', (req, res) => {})
 
 module.exports = server; // CommonJS modules (node)
+
+// const express = require('express');
+
+// const productsRouter = require('../products/products-router');
+// const suppliersRouter = require('../suppliers/suppliers-router');
+
+// const server = express();
+
+// // global middleware
+// server.use(express.json());
+
+// // using custom middleware globally:
+// // server.use(uppercaser);
+
+// // using middleware locally
+// server.use('/products', productsRouter);
+
+// // using custom middleware locally
+// server.use('/suppliers', uppercaser, suppliersRouter);
+
+// // route handlers
+// server.get('/', (req, res) => {
+//     res.send(`<h1>Test</h1>`);
+// });
+
+// // export default server
+// module.exports = server;
--- suppliers/suppliers-router.js
+++ suppliers/suppliers-router.js
@@ -3,13 +3,17 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json());
 
+function uppercaser () {
+    
+}
+
 router.get('/', (req, res) => {
     res.send('get to /suppliers ' + id)
 }
 )
-router.get('/:id', (req, res) => {
+router.get('/:name', (req, res) => {
     const { id } = req.params;
-    res.send(`get to /suppliers/${id}`);
+    res.send(`get to /suppliers/${req.params.name}`);
 })
 
 module.exports = router;
